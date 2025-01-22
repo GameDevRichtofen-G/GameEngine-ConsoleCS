30ed6106086fd9311ea065f9ba94e9e1e892d7a0
GameDevRichtofen-G <ai.edu.ac.mk@gmail.com>
2025-01-22 22:55:07 +0330

    commit

---
+++ GameEngine-C#RAW/something_random/Public_variables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    internal class Public_variables
+    {
+        public List<Main_class> Classes;
+        public Dictionary<string, string> sounds = new Dictionary<string, string>
+        {
+            { "music", "/Audio/pixel-245147.wav" }
+        };
+
+       
+
+        
+    }
+}
---
+++ GameEngine-C#RAW/something_random/Render_engine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    internal class Render_engine
+    {
+       
+        public void Render_engine_func(List<Main_class> Objects) 
+        {
+            //Refresh the entire console to render the next frame
+            Console.Clear();
+            
+            //check all the classes 
+            foreach (var obj in Objects)
+            {
+                //if this Main_class instance's(obj) Can_render is true, then we render the class 
+                if (obj.Can_render)
+                {
+                    //telling render engine where to draw the class
+                    Console.SetCursorPosition(obj.GetX(), obj.GetY());
+                    //setting the font color for this class
+                    Console.ForegroundColor = obj.color;
+                    //rendering the class
+                    Console.Write(obj.shape);
+                    
+
+                }
+            }
+        }
+    }
+}
